37055e948872d61b46e9e5a0bf5b60aae9a4b173
chq-matteo <chq.matteo@gmail.com>
2020-05-01 19:26:53 +0200

    Update __init__.py

--- torchcrf/__init__.py
+++ torchcrf/__init__.py
@@ -246,7 +246,7 @@ class CRF(nn.Module):
 
             # Set score to the next score if this timestep is valid (mask == 1)
             # shape: (batch_size, num_tags)
-            score = torch.where(mask[i].unsqueeze(1), next_score, score)
+            score = torch.where(mask[i].bool().unsqueeze(1), next_score, score)
 
         # End transition score
         # shape: (batch_size, num_tags)
